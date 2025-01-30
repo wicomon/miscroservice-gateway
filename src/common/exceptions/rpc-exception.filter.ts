93b14002dd7e7aff8e42a939f779f4a551cd5e65
wicomon <wcv.94@hotmail.com>
2025-01-30 11:07:00 -0500

    update: rcpExceptions custom message

--- src/common/exceptions/rpc-exception.filter.ts
+++ src/common/exceptions/rpc-exception.filter.ts
@@ -9,7 +9,15 @@ export class RpcCustomExceptionFilter implements ExceptionFilter {
     const response = ctx.getResponse();
 
     const rpcError = exception.getError();
-    // console.log(rpcError)
+    console.log(rpcError);
+
+    if( rpcError.toString().includes('Empty response') ){
+      return response.status(500).json({
+        statusCode: 500,
+        message: rpcError.toString().substring(0, rpcError.toString().indexOf('(') - 1),
+      });
+    }
+
     if (
       typeof rpcError === 'object' &&
       'status' in rpcError &&
