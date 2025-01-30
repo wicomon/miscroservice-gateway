54c5491b8a7106043dfb0ecd2e9d0b165e80123e
wicomon <wcv.94@hotmail.com>
2025-01-30 11:32:23 -0500

    update: custom init message

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
   app.useGlobalFilters(new  RpcCustomExceptionFilter )
 
   await app.listen(envs.port);
-
+  console.log('app running successfully')
   logger.log(`Gateway is running on: ${envs.port}`);
 }
 bootstrap();
